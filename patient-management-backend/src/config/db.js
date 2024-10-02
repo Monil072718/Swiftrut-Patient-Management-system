df4a47c74c96841a02c136aa8ffa4fcf4d557eac
panthigopani0987 <panthigopani1234@gmail.com>
2024-10-03 04:00:55 +0530

    db.js

--- patient-management-backend/src/config/db.js
+++ patient-management-backend/src/config/db.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 
-// MongoDB connection function
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI); // Remove the deprecated options
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
