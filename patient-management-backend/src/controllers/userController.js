fd2e9daa7114b2e2f027ba093163d72f2ca64537
panthigopani0987 <panthigopani1234@gmail.com>
2024-10-03 04:39:13 +0530

    forget password

--- patient-management-backend/src/controllers/userController.js
+++ patient-management-backend/src/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 
 //register a new user
 const register = async (req, res) => {
@@ -21,7 +22,7 @@ const register = async (req, res) => {
 };
 
 //login
-const login = ('/login', async (req, res) => {
+const login = async (req, res) => {
     const { emailOrPhone, password } = req.body;
     try {
         const user = await User.findOne({
@@ -37,9 +38,42 @@ const login = ('/login', async (req, res) => {
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
-})
+}
+
+const forgetPassword = async (req, res) => {
+    const { email } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: 'User not found' });
+
+        //reset token
+        const resetToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+        const transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: 'panthigopani1234@gmail.com',
+                pass: 'dojg zeme ylhd hjiw',
+            },
+        });
+
+        const mailOption = {
+            from: 'panthigopani1234@gmail.com',
+            to: email,
+            subject: 'Password Reset',
+            text: `You can reset your password using this link: http://localhost:3000/reset-password?token=${resetToken}`,
+        }
+
+        await transporter.sendMail(mailOption);
+        res.json({ message: 'Password reset email sent' });
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
 
 module.exports = {
     register,
-    login
+    login,
+    forgetPassword
 }
